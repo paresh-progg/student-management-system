ef105f29b29461bce4c232a754762bb89e202c19
paresh-progg <pareshsahoo528@gmail.com>
2026-06-27 11:36:29 +0530

    Improve project configuration and security

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "YOUR_MYSQL_PASSWORD",
-    database: "sms"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
@@ -12,7 +12,6 @@ db.connect((err) => {
         console.error("Database Connection Failed:", err);
         return;
     }
-
     console.log("MySQL Connected");
 });
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("******** THIS IS MY SERVER.JS ********");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
