7697c9b75ebb630247a6c7de0b9fee69a5368f99
paresh-progg <pareshsahoo528@gmail.com>
2026-06-30 11:17:19 +0530

    Configure Railway database connection

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
@@ -12,6 +13,7 @@ db.connect((err) => {
         console.error("Database Connection Failed:", err);
         return;
     }
+
     console.log("MySQL Connected");
 });
 
